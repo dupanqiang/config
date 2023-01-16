8f2642b9ce54485fb1b55166a36d78578ce5da52
zhao <v-zhaoyongfei@doublefs.com>
2023-01-16 11:02:02 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @author: zhao yongfei
  * @Date: 2023-01-09 11:53:20
  * @description: 
- * @LastEditTime: 2023-01-14 11:48:02
+ * @LastEditTime: 2023-01-14 14:07:23
  * @LastEditors: zhao yongfei
  * @FilePath: /dfs-page-config/src/App.vue
 -->
@@ -13,11 +13,9 @@
 </template>
 <script setup lang="ts">
   import { useStore } from "vuex";
-  const baseUrl = import.meta.env.VITE_APP_API;
   import Example from '@/views/example/index.vue';
   // import Example2 from '@/views/example2/index.vue';
   const store = useStore();
-  store.state._BASE_URL = baseUrl;
 </script>
 <style>
 html, body {
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-14 20:49:39
- * @LastEditTime: 2023-01-14 13:59:18
+ * @LastEditTime: 2023-01-14 14:07:34
  * @LastEditors: zhao yongfei
  * @Description: In User Settings Edit
  * @FilePath: /dfs-page-config/src/store/index.ts
@@ -16,7 +16,7 @@ export default createStore({
   state: {
     // 页面配置数据
     _CONFIG_DATA: {},
-    _BASE_URL: ""
+    _BASE_URL: import.meta.env.VITE_APP_API
   },
   getters: {
     // 获取页面配置数据
